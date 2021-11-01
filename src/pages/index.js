529ea784319ec5bbb79ce748e31e07b73c5ce155
Stackbit Code Editor <projects@stackbit.com>
2021-11-01 21:15:08 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,8 @@ const IndexPage = () => (
               Hi! I'm <strong>Julian Castro</strong>
               <br />
             </h2>
-            <p>Im a.</p>
+            <p>I'm not a nerd, I'm a intellectual Rockstar                                                          
+   Data Science and Physical Engineering Student.</p>
           </header>
 
           <footer>
