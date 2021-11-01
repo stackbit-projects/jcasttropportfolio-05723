64027c72edb1e407a5f1cf08c8eebad9ecb59491
Stackbit Code Editor <projects@stackbit.com>
2021-11-01 21:12:04 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,10 +53,7 @@ const IndexPage = () => (
           </header>
 
           <p>
-            Life will feel it is always a great need for eu valley, the valley
-            CNN ridiculous smile at any time chat mainstream clinical homes.
-            Mauris floor was very warm and we need it. One customer now nibh
-            Bureau dark pools behavior.
+            
           </p>
 
           <div className="row">
